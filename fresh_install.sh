79f0728e3b2b04e080831649f18701118cad3825
Juan <kipvasq@gmail.com>
2018-11-21 02:12:52 -0600

    ros installation

--- fresh_install.sh
+++ fresh_install.sh
@@ -9,7 +9,7 @@ sudo apt-get install nvidia-390 nvidia-settings
 # pc monitoring
 sudo apt install htop
 sudo apt install lm-sensors hddtemp
-sudo sensors-detect
+#sudo sensors-detect
 sensors
 sudo apt install psensor
 
@@ -29,3 +29,14 @@ sudo pip3 install rospkg
 sudo pip3 install cantools
 sudo pip3 install can
 sudo apt install can-utils
+
+# ros install
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
